b9324afc250fbca19b77f63e6720ff97459f9821
starlysamax <149339085+starlysamax@users.noreply.github.com>
2024-08-30 15:00:55 -0400

    new budget form

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,4 @@
+
 // rrd imports
 import { useLoaderData } from "react-router-dom";
 
@@ -6,6 +7,7 @@ import { toast } from "react-toastify";
 
 // components
 import Intro from "../components/Intro";
+import AddBudgetForm from "../components/AddBudgetForm";
 
 //  helper functions
 import { fetchData } from "../helpers"
@@ -13,7 +15,8 @@ import { fetchData } from "../helpers"
 // loader
 export function dashboardLoader() {
   const userName = fetchData("userName");
-  return { userName }
+  const budgets = fetchData("budgets");
+  return { userName, budgets }
 }
 
 // action
@@ -21,7 +24,7 @@ export async function dashboardAction({ request }) {
   const data = await request.formData();
   const formData = Object.fromEntries(data)
   try {
-    localStorage.setItem("userName", JSON.stringify(formData.userName));
+    localStorage.setItem("userName", JSON.stringify(formData.userName))
     return toast.success(`Welcome, ${formData.userName}`)
   } catch (e) {
     throw new Error("There was a problem creating your account.")
@@ -29,11 +32,23 @@ export async function dashboardAction({ request }) {
 }
 
 const Dashboard = () => {
-  const { userName } = useLoaderData()
+  const { userName, budgets } = useLoaderData()
 
   return (
     <>
-      {userName ? (<p>{userName}</p>) : <Intro />}
+      {userName ? (
+        <div className="dashboard">
+          <h1>Welcome back, <span className="accent">{userName}</span></h1>
+          <div className="grid-sm">
+            {/* {budgets ? () : ()} */}
+            <div className="grid-lg">
+              <div className="flex-lg">
+                <AddBudgetForm />
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : <Intro />}
     </>
   )
 }
