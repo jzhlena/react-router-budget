177cb10bc32367892c34108b3234a3cb30123c36
starlysamax <149339085+starlysamax@users.noreply.github.com>
2024-08-30 16:59:02 -0400

    handling multiple form submissions

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,8 @@
+const generateRandomColor = () => {
+    const existingBudgetLength = fetchData("budgets")?.length ?? 0;
+    return `${existingBudgetLength * 34} 65% 50%`
+  }
+
 // Local storage functions
 
 export const fetchData = (key) => {
@@ -5,7 +10,23 @@ export const fetchData = (key) => {
 }
 // ^ fetches any data given a key, in local storage and returns it
 
+//create budget
+export const createBudget = ({
+    name, amount
+  }) => {
+    const newItem = {
+      id: crypto.randomUUID(),
+      name: name,
+      createdAt: Date.now(),
+      amount: +amount,
+      color: generateRandomColor()
+    }
+    const existingBudgets = fetchData("budgets") ?? [];
+    return localStorage.setItem("budgets",
+      JSON.stringify([...existingBudgets, newItem]))
+  }
+
 // delete item
 export const deleteItem = ({ key }) => {
     return localStorage.removeItem(key)
-  }
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,3 @@
-
 // rrd imports
 import { useLoaderData } from "react-router-dom";
 
@@ -10,46 +9,58 @@ import Intro from "../components/Intro";
 import AddBudgetForm from "../components/AddBudgetForm";
 
 //  helper functions
-import { fetchData } from "../helpers"
+import { createBudget, fetchData } from "../helpers"
 
 // loader
 export function dashboardLoader() {
-  const userName = fetchData("userName");
-  const budgets = fetchData("budgets");
-  return { userName, budgets }
+    const userName = fetchData("userName");
+    const budgets = fetchData("bjdgets");
+    return { userName, budgets }
 }
 
 // action
 export async function dashboardAction({ request }) {
-  const data = await request.formData();
-  const formData = Object.fromEntries(data)
-  try {
-    localStorage.setItem("userName", JSON.stringify(formData.userName))
-    return toast.success(`Welcome, ${formData.userName}`)
-  } catch (e) {
-    throw new Error("There was a problem creating your account.")
-  }
+    const data = await request.formData();
+    const { _action, ...values } = Object.fromEntries(data)
+    // new user submission
+    if (_action === "newUser") {
+        try {
+            localStorage.setItem("userName", JSON.stringify(values.userName));
+            return toast.success(`Welcome, ${values.userName}`)
+        } catch (e) {
+            throw new Error("There was a problem creating your account.")
+        }
+    }
+    if (_action === "createBudget") {
+        try {
+            createBudget({
+                name: values.newBudget,
+                amount: values.newBudgetAmount,
+            })
+            return toast.success("Budget created!")
+        } catch (e) {
+            throw new Error("There was a problem creating your budget.")
+        }
+    }
 }
 
 const Dashboard = () => {
-  const { userName, budgets } = useLoaderData()
+    const { userName, budgets } = useLoaderData()
 
-  return (
-    <>
-      {userName ? (
-        <div className="dashboard">
-          <h1>Welcome back, <span className="accent">{userName}</span></h1>
-          <div className="grid-sm">
-            {/* {budgets ? () : ()} */}
-            <div className="grid-lg">
-              <div className="flex-lg">
-                <AddBudgetForm />
-              </div>
-            </div>
-          </div>
-        </div>
-      ) : <Intro />}
-    </>
-  )
+    return (
+        <>
+            {userName ? (<div className="dashboard">
+                <h1> Welcome back, <span className="accent">{userName}</span></h1>
+                <div className="grid-sm">
+                    {/* {budgets ? () : ()} */}
+                    <div className="grid-lg">
+                        <div className="flex-lg">
+                            <AddBudgetForm />
+                        </div>
+                    </div>
+                </div>
+            </div>) : <Intro />}
+        </>
+    )
 }
 export default Dashboard
