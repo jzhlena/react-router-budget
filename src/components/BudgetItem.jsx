14c4c542b0b13b8754a115134271eabde36db558
starlysamax <149339085+starlysamax@users.noreply.github.com>
2024-09-03 16:35:03 -0400

    add delete buttons to cards

--- src/components/BudgetItem.jsx
+++ src/components/BudgetItem.jsx
@@ -1,7 +1,9 @@
 //helper functions
+import { Form, Link } from "react-router-dom";
 import { calculateSpentByBudget, formatCurrency, formatPercentage } from "../helpers";
+import { BanknotesIcon, TrashIcon } from "@heroicons/react/24/solid";
 
-const BudgetItem = ({ budget }) => {
+const BudgetItem = ({ budget, showDelete = false }) => {
     const { id, name, amount, color } = budget;
     const spent = calculateSpentByBudget(id);
     return <div className="budget"
@@ -19,6 +21,27 @@ const BudgetItem = ({ budget }) => {
             <small>{formatCurrency(spent)} spent</small>
             <small>{formatCurrency(amount - spent)} remaining</small>
         </div>
+        {
+            showDelete ? (
+                <Form method="post" action="delete" onSubmit={(event) =>{
+                    if(!confirm("Are you sure you want to permanently delete this budget?")){
+                        event.preventDefault();
+                    }
+                }}>
+                    <button tupe="submit" className="btn">
+                        <span>Delete Budget</span>
+                        <TrashIcon width={20}/>
+                    </button>
+                </Form>
+            ) : (
+                <div className="flex-sm">
+                <Link to={`/budget/${id}`} className="btn">
+                <span>View Details</span>
+                <BanknotesIcon width={20}/>
+                </Link>
+                </div>
+            )
+        }
     </div>
 }
 
