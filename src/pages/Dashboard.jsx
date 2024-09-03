342fa709c3b7d3f8c41b4cbab6f00a4dd72f6754
starlysamax <149339085+starlysamax@users.noreply.github.com>
2024-09-03 13:25:38 -0400

    add budget links

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -12,7 +12,7 @@ import BudgetItem from "../components/BudgetItem";
 import Table from "../components/Table";
 
 //  helper functions
-import { createBudget, createExpense, fetchData, wait } from "../helpers"
+import { createBudget, createExpense, deleteItem, fetchData, wait } from "../helpers"
 
 // loader
 export function dashboardLoader() {
@@ -48,17 +48,28 @@ export async function dashboardAction({ request }) {
             throw new Error("There was a problem creating your budget.")
         }
     }
-    if(_action === "createExpense"){
-        try{
+    if (_action === "createExpense") {
+        try {
             //create an expense
-                createExpense({
-                    name: values.newExpense,
-                    amount: values.newExpenseAmount,
-                    budgetId: values.newExpenseBudget
-                })
+            createExpense({
+                name: values.newExpense,
+                amount: values.newExpenseAmount,
+                budgetId: values.newExpenseBudget
+            })
             return toast.success(`Expense ${values.newExpense} created!`)
-        } catch(e){
+        } catch (e) {
             throw new Error("There was a problem creating your expense.")
+        }
+    }
+    if (_action === "deleteExpense") {
+        try {
+            deleteItem({
+                key: "expenses",
+                id: values.expenseId,
+            });
+            return toast.success("Expense deleted!");
+        } catch (e) {
+            throw new Error("There was a problem deleting your expense.");
         }
     }
 }
@@ -72,40 +83,40 @@ const Dashboard = () => {
                 <h1> Welcome back, <span className="accent">{userName}</span></h1>
                 <div className="grid-sm">
                     {
-                    budgets && budgets.length > 0 ?
-                    (<div className="grid-lg">
-                        <div className="flex-lg">
-                            <AddBudgetForm />
-                            <AddExpenseForm budgets={budgets}/>
-                        </div>
-                        <h2> Existing Budgets</h2>
-                        <div className="budgets">
-                            {
-                                budgets.map((budget) => (
-                                    <BudgetItem key={budget.id} budget={budget} />
-                                ))
-                            }
-                        </div>
-                        {
-                            expenses && expenses.length > 0 && (
-                                <div className="grid-md">
-                                    <h2>Recent Expenses</h2>
-                                    <Table expenses={expenses.sort((a,b) => b.createdAt - a.createdAt).slice(0,8)}/>
-                                {expenses.length > 8 && (
-                                    <Link to="expenses"
-                                    className="btn btn--dark">
-                                        View all expenses
-                                    </Link>
-                                )}
+                        budgets && budgets.length > 0 ?
+                            (<div className="grid-lg">
+                                <div className="flex-lg">
+                                    <AddBudgetForm />
+                                    <AddExpenseForm budgets={budgets} />
+                                </div>
+                                <h2> Existing Budgets</h2>
+                                <div className="budgets">
+                                    {
+                                        budgets.map((budget) => (
+                                            <BudgetItem key={budget.id} budget={budget} />
+                                        ))
+                                    }
+                                </div>
+                                {
+                                    expenses && expenses.length > 0 && (
+                                        <div className="grid-md">
+                                            <h2>Recent Expenses</h2>
+                                            <Table expenses={expenses.sort((a, b) => b.createdAt - a.createdAt).slice(0, 8)} />
+                                            {expenses.length > 8 && (
+                                                <Link to="expenses"
+                                                    className="btn btn--dark">
+                                                    View all expenses
+                                                </Link>
+                                            )}
+                                        </div>
+                                    )}
+                            </div>) : (
+                                <div className="grid-sm">
+                                    <p> Personal budgeting is the secret to financial freedom</p>
+                                    <p>Create a budget to get started!</p>
+                                    <AddBudgetForm />
                                 </div>
                             )}
-                    </div>) : (
-                        <div className="grid-sm">
-                            <p> Personal budgeting is the secret to financial freedom</p>
-                            <p>Create a budget to get started!</p>
-                            <AddBudgetForm />
-                        </div>
-                    )}
                 </div>
             </div>) : <Intro />}
         </>
